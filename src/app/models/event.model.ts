34aedddf147392f6ea13b1a6c0ce9d02b6ae5088
joordiii <joordiii@gmail.com>
2017-12-12 13:08:57 +0100

    .

--- src/app/models/event.model.ts
+++ src/app/models/event.model.ts
@@ -1,16 +1,17 @@
 export class Event {
 
-    user_id: object;
+    user_id: {
+        organizationName: string;
+        myAddress: string;
+        myTelephone: string;
+        myEmail: string;
+        myWeb: string;
+    };
     slogan: string;
     startDate: string;
     endDate: string;
     place: string;
     description: string;
-    organizationName: string;
-    myAddress: string;
-    myTelephone: string;
-    myEmail: string;
-    myWeb: string;
 
     constructor(obj: Object = {}) {
       Object.assign(this, obj);
