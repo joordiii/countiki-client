83437ca8df8f8f1a4d45f02960fd9a98bea6f3eb
joordiii <joordiii@gmail.com>
2017-12-17 20:58:57 +0100

    circle drawn ok

--- src/app/components/event-long/event-long.component.ts
+++ src/app/components/event-long/event-long.component.ts
@@ -5,6 +5,10 @@ import { EventComponent } from '../../pages/event/event.component';
 import { EventService } from '../../services/event.service';
 import { Event } from '../../models/event.model';
 import { MapsAPILoader } from '@agm/core';
+import { AgmCoreModule } from '@agm/core';
+import { AgmCircle } from '@agm/core/directives/circle';
+import { AgmPolygon } from '@agm/core/directives/polygon';
+import { AgmPolyline } from '@agm/core/directives/polyline';
 
 @Component({
   selector: 'app-event-long',
@@ -31,72 +35,89 @@ export class EventLongComponent implements OnInit {
     attendance: ''
   });
 
-  public latitude: Number;
-  public longitude: Number;
+  public latitude: number;
+  public longitude: number;
   public searchControl: FormControl;
   public zoom: number;
+  public iconUrl = 'https://image.flaticon.com/icons/png/128/145/145602.png';
+  public fillColor = '#35aa00';
+  public fillOpacity = 0.5;
+  public radius = 300;
 
- @ViewChild('search')
+ 
+  @ViewChild('circle', {read: AgmCircle})
+  public searchCircle: AgmCircle;
+
+  @ViewChild('search') 
   public searchElementRef: ElementRef;
 
   constructor(private mapsAPILoader: MapsAPILoader, private ngZone: NgZone, private router: Router, private eventService: EventService) { }
 
   ngOnInit() {
 
-        this.getEventId(this.eventId);
+    // get bounds of the circle
+    console.log(this.searchCircle);
+    /* this.searchCircle.getBounds().then((bounds) => {
+      console.log(bounds);
+    })
+    .catch((err) => {
+      console.log(err.message);
+    }); */
 
-        // set google maps defaults
-        this.zoom = 15;
+    this.getEventId(this.eventId);
+
+    // set google maps defaults
+    this.zoom = 15;
 
 
-        // create search FormControl
-        this.searchControl = new FormControl();
+    // create search FormControl
+    this.searchControl = new FormControl();
 
-        // load Places Autocomplete
-        this.mapsAPILoader.load().then(() => {
-          const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
-            types: ['address']
-          });
-          autocomplete.addListener('place_changed', () => {
-            this.ngZone.run(() => {
-              // get the place result
-              const place: google.maps.places.PlaceResult = autocomplete.getPlace();
+    // load Places Autocomplete
+    this.mapsAPILoader.load().then(() => {
+      const autocomplete = new google.maps.places.Autocomplete(this.searchElementRef.nativeElement, {
+        types: ['address']
+      });
+      autocomplete.addListener('place_changed', () => {
+        this.ngZone.run(() => {
+          // get the place result
+          const place: google.maps.places.PlaceResult = autocomplete.getPlace();
 
-              // verify result
-              if (place.geometry === undefined || place.geometry === null) {
-                return;
-              }
-              this.latitude = place.geometry.location.lat();
-              this.longitude = place.geometry.location.lng();
-              this.zoom = 15;
-
-            });
-          });
-        });
-      }
-
-      getEventId(id) {
-        this.eventService.getById(id)
-          .subscribe((data: Event) => {
-            this.data = data;
-            console.log('map', this.data);
-            console.log('map2', this.data.attendance);
-            this.latitude = this.data.location.coordinates[0] ;
-            this.longitude = this.data.location.coordinates[1];
-          });
-      }
-
-      addAttendeeEvent() {
-          const add = {
-          location: {
-            coordinates: [this.latitude, this.longitude]
+          // verify result
+          if (place.geometry === undefined || place.geometry === null) {
+            return;
           }
-        };
+          this.latitude = place.geometry.location.lat();
+          this.longitude = place.geometry.location.lng();
+          this.zoom = 15;
 
-        this.eventService.putAttendee(this.eventId, add)
-        .subscribe(
-          () => this.router.navigate(['/success']));
-          console.log();
+        });
+      });
+    });
+  }
+
+  getEventId(id) {
+    this.eventService.getById(id)
+      .subscribe((data: Event) => {
+        this.data = data;
+        console.log('map', this.data);
+        console.log('map2', this.data.attendance);
+        this.latitude = this.data.location.coordinates[0] ;
+        this.longitude = this.data.location.coordinates[1];
+      });
+  }
+
+  addAttendeeEvent() {
+      const add = {
+      location: {
+        coordinates: [this.latitude, this.longitude]
       }
+    };
+
+    this.eventService.putAttendee(this.eventId, add)
+    .subscribe(
+      () => this.router.navigate(['/success']));
+      console.log();
+  }
 
 }
